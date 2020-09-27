20779a642a902b1f2fcb18b338c3fba11011e22d
Christian <christian@guzzy.dk>
2020-09-27 10:02:46 +0200

    fixed the endless loop, and the script is now capable of searching through all folders in the working directory

--- search.py
+++ search.py
@@ -3,9 +3,11 @@ import os.path
 file_extensions = ['.doc','.pdf','docx','.dot','.dotm','.xls','.xlsx','.ppt','.pptx','.gif','.exe','.png','.psd','.txt','.html']
 list_files = []
 list_folders = []
+searched_folders = []
 def search_file():
     try:
         working_dir = os.path.abspath(os.getcwd())
+        searched_folders.append(working_dir)
         list_dir = os.listdir('.') #get a 'list' of files and dir's within the current directory
         for file in list_dir: #loop through the files in the list
             for i in range(len(file_extensions)): #get the index
@@ -13,7 +15,6 @@ def search_file():
                     list_files.append(working_dir + '/' + file) #append to list of files
             if os.path.isdir(file):
                     list_folders.append(working_dir + '/' + file) #append found folders to a list
-        #print("Files containing stored extension: " + str(list_files))
         #print("Folders: " + str(list_folders))
     except IOError:
         print("There was an error with " + SystemError)
@@ -21,20 +22,20 @@ def search_file():
 def change_dir():
     try:
         for folder in list_folders:
-            for i in range(len(list_folders)):
-                os.chdir(list_folders[i]) #change current working directory
-                #print("folder changed to " + os.getcwd())
-                search_file()
+            if folder in searched_folders: #if the folder has already been searched 
+                break
+            else:
+                for i in range(len(list_folders)):
+                    os.chdir(list_folders[i]) #change current working directory
+                    #print("Searching folder: " + os.getcwd())
+                    search_file()
     except:
         print("There was an error with " + SystemError)
 search_file()
 change_dir()
+print("Files found: " + str(list_files))
 
 
-#make search into a function
-    # Add parameter to specify which folder to search
-#add function to go into folders
-# change folder with os.chdir(path)
+# Add parameter to specify which folder to search
 # make the output of the matched files beautiful
-
 
